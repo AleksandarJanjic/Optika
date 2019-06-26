24183fdd36fd3d17e3855b1f0ad407bcbcf085f8
AleksandarJanjic <bata_leka@yahoo.com>
2019-06-26 18:39:33 +0200

    First

---
+++ src/main/java/com/Optika/OptikaApplication.java
@@ -0,0 +1,13 @@
+package com.Optika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Optika/OptikaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Optika;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OptikaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
